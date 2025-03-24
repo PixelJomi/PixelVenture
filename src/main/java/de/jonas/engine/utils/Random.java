bc01fa9f80ab27cec52c423f4113136a1cbee394
Jonas <coding.jonas.git@gmail.com>
2025-03-24 17:13:40 +0100

    Updated Random.java to be public

--- src/main/java/de/jonas/engine/utils/Random.java
+++ src/main/java/de/jonas/engine/utils/Random.java
@@ -4,10 +4,10 @@ import java.math.BigDecimal;
 import java.util.concurrent.ThreadLocalRandom;
 
 @SuppressWarnings("unused") public class Random {
-    static int randInt(int minVal, int maxVal) {
+    public static int randInt(int minVal, int maxVal) {
         return ThreadLocalRandom.current().nextInt(minVal, maxVal + 1);
     }
-    static float randFloat(float min, float max) {
+    public static float randFloat(float min, float max) {
         return ThreadLocalRandom.current().nextFloat() * (max - min + Float.MIN_VALUE) + min;
     }
     public static boolean chance(double pct) {
