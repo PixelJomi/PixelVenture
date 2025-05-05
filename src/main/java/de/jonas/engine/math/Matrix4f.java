9cbe0e9fa80f56c69053a7227f620d91413d898e
JomiCraft <melcstadia@gmail.com>
2025-05-05 14:51:20 +0200

    I added Uniforms to Shaders

---
+++ src/main/java/de/jonas/engine/math/Matrix4f.java
@@ -0,0 +1,16 @@
+package de.jonas.engine.math;
+
+public class Matrix4f {
+    public static final int SIZE = 4;
+    private float[] elements;
+
+    public float get(int x, int y) {
+        return elements[y * SIZE + x];
+    }
+
+    public void set(int x, int y, float value) {
+        elements[y * SIZE + x] = value;
+    }
+
+    public float[] getAll() {return elements;}
+}
--- src/main/resources/shaders/mainVertex.glsl
+++ src/main/resources/shaders/mainVertex.glsl
@@ -7,8 +7,10 @@ in vec2 textureCord;
 out vec3 passColor;
 out vec2 passTextureCord;
 
+uniform float scale;
+
 void main() {
-    gl_Position = vec4(position, 1.0);
+    gl_Position = vec4(position, 1.0) * vec4(scale, scale, scale, 1);
     passColor = color;
     passTextureCord = textureCord;
 }
